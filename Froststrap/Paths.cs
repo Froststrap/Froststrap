f87bc7874b86b97771b8b53aa492023550f76bd3
UltimatePlayer97 <gogole.help134@gmail.com>
2026-02-05 18:41:40 +1100

    add linux specific config paths

--- Froststrap/Paths.cs
+++ Froststrap/Paths.cs
@@ -1,25 +1,27 @@
-﻿namespace Froststrap
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Froststrap
 {
     static class Paths
     {
-        // note that these are directories that aren't tethered to the basedirectory
-        // so these can safely be called before initialization
         public static string Temp => Path.Combine(Path.GetTempPath(), App.ProjectName);
         public static string UserProfile => Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         public static string LocalAppData => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         public static string Desktop => Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
         public static string WindowsStartMenu => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs");
         public static string System => Environment.GetFolderPath(Environment.SpecialFolder.System);
-
         public static string Process => Environment.ProcessPath!;
 
         public static string TempUpdates => Path.Combine(Temp, "Updates");
         public static string TempLogs => Path.Combine(Temp, "Logs");
 
-        public static string Base { get; private set; } = "";
+        public static string ConfigRoot { get; private set; } = "";
+        public static string DataRoot { get; private set; } = "";
+
         public static string Downloads { get; private set; } = "";
         public static string Cache { get; private set; } = "";
-
         public static string SavedFlagProfiles { get; private set; } = "";
         public static string Logs { get; private set; } = "";
         public static string Integrations { get; private set; } = "";
@@ -34,26 +36,46 @@
 
         public static string CustomFont => Path.Combine(Modifications, "content", "fonts", "CustomFont.ttf");
 
-        public static bool Initialized => !String.IsNullOrEmpty(Base);
+        public static string Base => DataRoot;
+        public static bool Initialized => !String.IsNullOrEmpty(DataRoot);
 
         public static void Initialize(string baseDirectory)
         {
-            Base = baseDirectory;
-            Downloads = Path.Combine(Base, "Downloads");
-            SavedFlagProfiles = Path.Combine(Base, "SavedFlagProfiles");
-            Logs = Path.Combine(Base, "Logs");
-            Integrations = Path.Combine(Base, "Integrations");
-            Versions = Path.Combine(Base, "Versions");
-            Modifications = Path.Combine(Base, "Modifications");
-            Roblox = Path.Combine(LocalAppData, "Roblox");
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                ConfigRoot = baseDirectory;
+                DataRoot = baseDirectory;
+                Roblox = Path.Combine(LocalAppData, "Roblox");
+            }
+            else
+            {
+                // Tried to follow XDG Base Directory Specification
+                ConfigRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), App.ProjectName);
+                DataRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), App.ProjectName);
+
+
+                Roblox = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Roblox");
+            }
+
+            SavedFlagProfiles = Path.Combine(ConfigRoot, "SavedFlagProfiles");
+            CustomCursors = Path.Combine(ConfigRoot, "CustomCursorsSets");
+            CustomThemes = Path.Combine(ConfigRoot, "CustomThemes");
+
+            Downloads = Path.Combine(DataRoot, "Downloads");
+            Logs = Path.Combine(DataRoot, "Logs");
+            Integrations = Path.Combine(DataRoot, "Integrations");
+            Versions = Path.Combine(DataRoot, "Versions");
+            Modifications = Path.Combine(DataRoot, "Modifications");
+            Cache = Path.Combine(DataRoot, "Cache");
+
             RobloxLogs = Path.Combine(Roblox, "logs");
             RobloxCache = Path.Combine(Path.GetTempPath(), "Roblox");
-            CustomThemes = Path.Combine(Base, "CustomThemes");
-            CustomCursors = Path.Combine(Base, "CustomCursorsSets");
-            Cache = Path.Combine(Base, "Cache");
 
-            Application = Path.Combine(Base, $"{App.ProjectName}.exe");
+            string exeName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? $"{App.ProjectName}.exe" : App.ProjectName;
+            Application = Path.Combine(DataRoot, exeName);
+
+            Directory.CreateDirectory(ConfigRoot);
+            Directory.CreateDirectory(DataRoot);
         }
-
     }
 }
