20d8aed2395fe12f3a6ecfadf955209e2613a1d3
ItzBloxxy <thebestcheese25@gmail.com>
2026-03-17 18:33:59 +0200

    Clean up using directives in HttpClientEx.cs
    Removed unnecessary using directives from HttpClientEx.cs

--- Bloxstrap/Extensions/HttpClientEx.cs
+++ Bloxstrap/Extensions/HttpClientEx.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Bloxstrap.Extensions
 {
     internal static class HttpClientEx
