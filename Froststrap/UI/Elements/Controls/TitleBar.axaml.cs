ae427c054e1a0b6e5ed932c63b30577161195e82
RealMeddsam <realferidjelalia@gmail.com>
2026-03-18 22:32:48 +0100

    Fix up titlebar icons and colors

--- Froststrap/UI/Elements/Controls/TitleBar.axaml.cs
+++ Froststrap/UI/Elements/Controls/TitleBar.axaml.cs
@@ -1,7 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
-using System;
 
 namespace Froststrap.UI.Elements.Controls
 {
@@ -11,46 +10,26 @@ namespace Froststrap.UI.Elements.Controls
             AvaloniaProperty.Register<TitleBar, string?>(nameof(Title));
 
         public static readonly StyledProperty<bool> ShowMinimizeProperty =
-            AvaloniaProperty.Register<TitleBar, bool>(nameof(ShowMinimize), true);
+            AvaloniaProperty.Register<TitleBar, bool>(nameof(ShowMinimize), false);
 
         public static readonly StyledProperty<bool> ShowMaximizeProperty =
-            AvaloniaProperty.Register<TitleBar, bool>(nameof(ShowMaximize), true);
+            AvaloniaProperty.Register<TitleBar, bool>(nameof(ShowMaximize), false);
+
+        public static readonly StyledProperty<bool> CanMaximizeProperty =
+            AvaloniaProperty.Register<TitleBar, bool>(nameof(CanMaximize), false);
 
         public static readonly StyledProperty<bool> ShowCloseProperty =
-            AvaloniaProperty.Register<TitleBar, bool>(nameof(ShowClose), true);
+            AvaloniaProperty.Register<TitleBar, bool>(nameof(ShowClose), false);
 
         public static readonly StyledProperty<Avalonia.Media.IImage?> IconProperty =
             AvaloniaProperty.Register<TitleBar, Avalonia.Media.IImage?>(nameof(Icon));
 
-        public string? Title
-        {
-            get => GetValue(TitleProperty);
-            set => SetValue(TitleProperty, value);
-        }
-
-        public bool ShowMinimize
-        {
-            get => GetValue(ShowMinimizeProperty);
-            set => SetValue(ShowMinimizeProperty, value);
-        }
-
-        public bool ShowMaximize
-        {
-            get => GetValue(ShowMaximizeProperty);
-            set => SetValue(ShowMaximizeProperty, value);
-        }
-
-        public bool ShowClose
-        {
-            get => GetValue(ShowCloseProperty);
-            set => SetValue(ShowCloseProperty, value);
-        }
-
-        public Avalonia.Media.IImage? Icon
-        {
-            get => GetValue(IconProperty);
-            set => SetValue(IconProperty, value);
-        }
+        public string? Title { get => GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
+        public bool ShowMinimize { get => GetValue(ShowMinimizeProperty); set => SetValue(ShowMinimizeProperty, value); }
+        public bool ShowMaximize { get => GetValue(ShowMaximizeProperty); set => SetValue(ShowMaximizeProperty, value); }
+        public bool CanMaximize { get => GetValue(CanMaximizeProperty); set => SetValue(CanMaximizeProperty, value); }
+        public bool ShowClose { get => GetValue(ShowCloseProperty); set => SetValue(ShowCloseProperty, value); }
+        public Avalonia.Media.IImage? Icon { get => GetValue(IconProperty); set => SetValue(IconProperty, value); }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
@@ -61,19 +40,20 @@ namespace Froststrap.UI.Elements.Controls
 
             var dragLayer = e.NameScope.Find<Control>("PART_DragLayer");
             if (dragLayer != null)
-            {
                 dragLayer.PointerPressed += (s, ev) => window.BeginMoveDrag(ev);
-            }
 
             var minBtn = e.NameScope.Find<Button>("PART_MinimizeButton");
-            if (minBtn != null) minBtn.Click += (s, ev) => window.WindowState = Avalonia.Controls.WindowState.Minimized;
+            if (minBtn != null)
+                minBtn.Click += (s, ev) => window.WindowState = Avalonia.Controls.WindowState.Minimized;
 
             var maxBtn = e.NameScope.Find<Button>("PART_MaximizeButton");
-            if (maxBtn != null) maxBtn.Click += (s, ev) =>
-                window.WindowState = window.WindowState == Avalonia.Controls.WindowState.Maximized ? Avalonia.Controls.WindowState.Normal : Avalonia.Controls.WindowState.Maximized;
+            if (maxBtn != null)
+                maxBtn.Click += (s, ev) =>
+                    window.WindowState = window.WindowState == Avalonia.Controls.WindowState.Maximized ? Avalonia.Controls.WindowState.Normal : Avalonia.Controls.WindowState.Maximized;
 
             var closeBtn = e.NameScope.Find<Button>("PART_CloseButton");
-            if (closeBtn != null) closeBtn.Click += (s, ev) => window.Close();
+            if (closeBtn != null)
+                closeBtn.Click += (s, ev) => window.Close();
         }
     }
 }
