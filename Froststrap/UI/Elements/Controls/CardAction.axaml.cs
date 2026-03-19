d6180218b98c2befd1c7eb09a7aa0dd5679dd5a9
RealMeddsam <realferidjelalia@gmail.com>
2026-03-19 17:39:46 +0100

    Create card action

---
+++ Froststrap/UI/Elements/Controls/CardAction.axaml.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using IconPacks.Avalonia.Material;
+
+namespace Froststrap.UI.Elements.Controls
+{
+    public class CardAction : Button
+    {
+        public static readonly StyledProperty<string> HeaderProperty =
+            AvaloniaProperty.Register<CardAction, string>(nameof(Header));
+
+        public static readonly StyledProperty<string> DescriptionProperty =
+            AvaloniaProperty.Register<CardAction, string>(nameof(Description));
+
+        public static readonly StyledProperty<PackIconMaterialKind> IconProperty =
+            AvaloniaProperty.Register<CardAction, PackIconMaterialKind>(nameof(Icon));
+
+        public string Header
+        {
+            get => GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        public string Description
+        {
+            get => GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public PackIconMaterialKind Icon
+        {
+            get => GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+    }
+}
