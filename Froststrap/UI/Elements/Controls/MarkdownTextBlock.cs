f7f233c9ce8c50d0f7262c33bd2f2b73883d51c8
RealMeddsam <realferidjelalia@gmail.com>
2026-07-26 22:56:27 +0100

    Improve ui

--- Froststrap/UI/Elements/Controls/MarkdownTextBlock.cs
+++ Froststrap/UI/Elements/Controls/MarkdownTextBlock.cs
@@ -6,6 +6,8 @@ using Avalonia.Media;
 using Markdig;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
+using AvaloniaInline = Avalonia.Controls.Documents.Inline;
+using MarkdigInline = Markdig.Syntax.Inlines.Inline;
 
 namespace Froststrap.UI.Elements.Controls
 {
@@ -31,26 +33,33 @@ namespace Froststrap.UI.Elements.Controls
         static MarkdownTextBlock()
         {
             MarkdownTextProperty.Changed.AddClassHandler<MarkdownTextBlock>((x, e) => x.UpdateMarkdown(e.NewValue as string));
-
             FontSizeProperty.Changed.AddClassHandler<MarkdownTextBlock>((x, e) => x.UpdateMarkdown(x.MarkdownText));
+            FontFamilyProperty.Changed.AddClassHandler<MarkdownTextBlock>((x, e) => x.UpdateMarkdown(x.MarkdownText));
+            FontWeightProperty.Changed.AddClassHandler<MarkdownTextBlock>((x, e) => x.UpdateMarkdown(x.MarkdownText));
         }
 
         private void UpdateMarkdown(string? markdown)
         {
-            if (Inlines == null) return;
-            Inlines.Clear();
-
+            Inlines?.Clear();
             if (string.IsNullOrEmpty(markdown)) return;
 
             var document = Markdown.Parse(markdown, _markdownPipeline);
-            var result = ConvertMarkdownToInlines(document);
+            var result = ConvertMarkdownToInlines(
+                document,
+                FontSize,
+                FontFamily,
+                FontWeight);
 
-            Inlines.AddRange(result);
+            Inlines!.AddRange(result);
         }
 
-        private static List<Avalonia.Controls.Documents.Inline> ConvertMarkdownToInlines(MarkdownDocument document)
+        private static List<AvaloniaInline> ConvertMarkdownToInlines(
+            MarkdownDocument document,
+            double fontSize,
+            Avalonia.Media.FontFamily fontFamily,
+            FontWeight fontWeight)
         {
-            var inlines = new List<Avalonia.Controls.Documents.Inline>();
+            var inlines = new List<AvaloniaInline>();
 
             foreach (var block in document)
             {
@@ -58,21 +67,27 @@ namespace Froststrap.UI.Elements.Controls
                 {
                     foreach (var inline in paragraphBlock.Inline)
                     {
-                        var avaloniaInlines = GetAvaloniaInlinesFromMarkdownInline(inline);
+                        var avaloniaInlines = GetAvaloniaInlinesFromMarkdownInline(
+                            inline,
+                            fontSize,
+                            fontFamily,
+                            fontWeight);
                         inlines.AddRange(avaloniaInlines);
                     }
 
                     if (block != document.LastChild)
-                    {
                         inlines.Add(new LineBreak());
-                    }
                 }
             }
 
             return inlines;
         }
 
-        private static IEnumerable<Avalonia.Controls.Documents.Inline> GetAvaloniaInlinesFromMarkdownInline(Markdig.Syntax.Inlines.Inline? inline)
+        private static IEnumerable<AvaloniaInline> GetAvaloniaInlinesFromMarkdownInline(
+            MarkdigInline? inline,
+            double fontSize,
+            Avalonia.Media.FontFamily fontFamily,
+            FontWeight fontWeight)
         {
             if (inline == null) yield break;
 
@@ -96,7 +111,11 @@ namespace Froststrap.UI.Elements.Controls
 
                 foreach (var child in emphasisInline)
                 {
-                    foreach (var childInline in GetAvaloniaInlinesFromMarkdownInline(child))
+                    foreach (var childInline in GetAvaloniaInlinesFromMarkdownInline(
+                        child,
+                        fontSize,
+                        fontFamily,
+                        fontWeight))
                         span.Inlines.Add(childInline);
                 }
                 yield return span;
@@ -106,10 +125,13 @@ namespace Froststrap.UI.Elements.Controls
                 var url = linkInline.Url ?? string.Empty;
                 var linkText = linkInline.FirstChild?.ToString() ?? url;
 
-                var hyperlinkControl = new Hyperlink()
+                var hyperlinkControl = new Hyperlink
                 {
                     Content = linkText,
-                    Url = url
+                    Url = url,
+                    FontSize = fontSize,
+                    FontFamily = fontFamily,
+                    FontWeight = fontWeight,
                 };
 
                 yield return new InlineUIContainer(hyperlinkControl)
@@ -125,7 +147,11 @@ namespace Froststrap.UI.Elements.Controls
             {
                 foreach (var child in container)
                 {
-                    foreach (var childInline in GetAvaloniaInlinesFromMarkdownInline(child))
+                    foreach (var childInline in GetAvaloniaInlinesFromMarkdownInline(
+                        child,
+                        fontSize,
+                        fontFamily,
+                        fontWeight))
                         yield return childInline;
                 }
             }
