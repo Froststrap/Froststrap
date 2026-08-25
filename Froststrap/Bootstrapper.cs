4ab4d94c56850e3ebb4ad7376c05bd310607ae61
Invra <identificationsucks@gmail.com>
2026-08-25 13:01:17 +1000

    chore: remove ImageSharp

--- Froststrap/Bootstrapper.cs
+++ Froststrap/Bootstrapper.cs
@@ -20,7 +20,6 @@ using ICSharpCode.SharpZipLib.GZip;
 using ICSharpCode.SharpZipLib.Tar;
 using ICSharpCode.SharpZipLib.Zip;
 using Microsoft.Win32;
-// using SixLabors.ImageSharp;
 using System.ComponentModel;
 using System.Data;
 using System.Net.Http.Json;
