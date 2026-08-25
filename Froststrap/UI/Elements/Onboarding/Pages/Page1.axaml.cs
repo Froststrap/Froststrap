6da6b3a84a59350e0b5c10abe2d9669ca948a511
Invra <identificationsucks@gmail.com>
2026-08-25 22:27:34 +1000

    chore: cool little icon cycling

--- Froststrap/UI/Elements/Onboarding/Pages/Page1.axaml.cs
+++ Froststrap/UI/Elements/Onboarding/Pages/Page1.axaml.cs
@@ -1,14 +1,59 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Froststrap.UI.ViewModels.Onboarding;
+using LucideAvalonia.Enum;
 
 namespace Froststrap.UI.Elements.Onboarding.Pages
 {
     public partial class Page1 : UserControl
     {
+        private static readonly LucideIconNames[] IconCycle = [LucideIconNames.Languages, LucideIconNames.Globe];
+        private static readonly TimeSpan HoldDuration = TimeSpan.FromSeconds(5);
+        private static readonly TimeSpan FadeDuration = TimeSpan.FromSeconds(1);
+
+        private CancellationTokenSource? _cts;
+
         public Page1()
         {
             DataContext = new Page1ViewModel();
             InitializeComponent();
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
+        }
+
+        private void OnLoaded(object? sender, RoutedEventArgs e)
+        {
+            _cts = new CancellationTokenSource();
+            _ = RunIconCycleAsync(_cts.Token);
+        }
+
+        private void OnUnloaded(object? sender, RoutedEventArgs e)
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+            _cts = null;
+        }
+
+        private async Task RunIconCycleAsync(CancellationToken token)
+        {
+            var index = 0;
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    await Task.Delay(HoldDuration, token);
+
+                    HeaderIcon.Opacity = 0;
+                    await Task.Delay(FadeDuration, token);
+
+                    index = (index + 1) % IconCycle.Length;
+                    HeaderIcon.Icon = IconCycle[index];
+
+                    HeaderIcon.Opacity = 1;
+                    await Task.Delay(FadeDuration, token);
+                }
+            }
+            catch (TaskCanceledException) { }
         }
     }
 }
