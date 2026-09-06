1c2defae47bce856e3ef9c275d763e144be26014
Invra <identificationsucks@gmail.com>
2026-09-07 01:08:05 +1000

    nix: make avdt work on linux

--- nix/avdt.nix
+++ nix/avdt.nix
@@ -1,25 +1,38 @@
 {
-  buildDotnetGlobalTool,
-  writeShellScriptBin,
+  lib,
+  stdenv,
   dotnetCorePackages,
+  writeShellScriptBin,
+  buildDotnetGlobalTool
 }:
 let
-  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-sdk = dotnetCorePackages.sdk_10_0;
   avdt-unwrapped = buildDotnetGlobalTool {
     pname = "avdt";
     version = "2.2.3";
-    nugetSha256 = "K0cjbRCBabODRwua7BsNEEO++4jPNIsW9+bwHDtsNz8=";
-    nugetName = "avaloniaui.developertools.macos";
+    nugetSha256 = if stdenv.hostPlatform.isDarwin then
+      "K0cjbRCBabODRwua7BsNEEO++4jPNIsW9+bwHDtsNz8="
+    else if stdenv.hostPlatform.isLinux then
+      "e46swp/RGRZXmdremASzqQ6+Qrs90gabrY5lWP7y/p8="
+    else "";
+    nugetName =
+      "avaloniaui.developertools."
+      + lib.optionalString stdenv.hostPlatform.isLinux "linux"
+      + lib.optionalString stdenv.hostPlatform.isDarwin "macos";
     dotnet-sdk = dotnet-sdk;
   };
 in
-writeShellScriptBin "avdt" ''
-  DATA_DIR="''${XDG_DATA_HOME:-$HOME/.local/share}/avdt-2.2.3"
-  if [ ! -d "$DATA_DIR" ]; then
-    mkdir -p "$DATA_DIR"
-    cp -r ${avdt-unwrapped}/lib/avdt/. "$DATA_DIR"
-    chmod -R u+w "$DATA_DIR"
-  fi
-  export DOTNET_ROOT="${dotnet-sdk}/share/dotnet"
-  exec "$DATA_DIR/avdt" "$@"
-''
+writeShellScriptBin "avdt" (
+  if stdenv.hostPlatform.isDarwin then ''
+    DATA_DIR="''${XDG_DATA_HOME:-$HOME/.local/share}/avdt-2.2.3"
+    if [ ! -d "$DATA_DIR" ]; then
+      mkdir -p "$DATA_DIR"
+      cp -r ${avdt-unwrapped}/lib/avdt/. "$DATA_DIR"
+      chmod -R u+w "$DATA_DIR"
+    fi
+    export DOTNET_ROOT="${dotnet-sdk}/share/dotnet"
+    exec "$DATA_DIR/avdt" "$@"
+  '' else ''
+    exec ${avdt-unwrapped}/bin/avdt "$@"
+  ''
+)
